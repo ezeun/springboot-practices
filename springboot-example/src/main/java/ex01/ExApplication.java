4d80e226c373c9034611dbabf5f0d2b1b318a818
ezeun <consistency1231@gmail.com>
2025-01-20 16:56:32 +0900

    springboot #1

--- springboot-example/src/main/java/ex01/ExApplication.java
+++ springboot-example/src/main/java/ex01/ExApplication.java
@@ -1,13 +1,35 @@
 package ex01;
 
+import java.util.Map;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class ExApplication {
+public class ExApplication { // Bootstrapping Class
 
+	@Bean
+	public Map<?,?> myMap(){
+		return Map.of("greetings", "Hello World");
+	}
 	public static void main(String[] args) {
-		SpringApplication.run(ExApplication.class, args);
+		/*
+		ConfigurableApplicationContext ac = null;
+		
+		try {
+			ac = SpringApplication.run(ExApplication.class, args);		
+			System.out.println("ExApplication runs ..." + ac.getBean("myMap", Map.class).get("greetings"));
+		} catch(Exception ignored) {
+		} finally {
+			ac.close();
+		}
+		*/
+		try(ConfigurableApplicationContext ac =SpringApplication.run(ExApplication.class, args)){
+			System.out.println("ExApplication runs ..." + ac.getBean("myMap", Map.class).get("greetings"));
+		}
 	}
 
 }
